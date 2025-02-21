5f355108b36e66d87eb0a499e3b073309d2e06b1
HimmatSingh9340 <sin14257@sheridancollege.ca>
2025-02-20 19:29:49 -0500

    new file

---
+++ src/javapracticeweek1/Javapracticeweek1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javapracticeweek1;
+
+/**
+ *
+ * @author Himmat singh
+ */
+public class Javapracticeweek1{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentlist2 = new Student[2];
+
+    studentlist2[0] = new Student();
+    studentlist2[0].setName("Harry");
+
+    studentlist2[1] = new Student();
+    studentlist2[1].setName("Himmat");
+
+    for(Student s: studentlist2){
+        System.out.println(s.getName());
+    }
+    }
+    
+}
